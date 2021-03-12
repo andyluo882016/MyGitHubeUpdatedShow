ed1936b3144160f7a674fabbebc3267e9fb11a21
andyluo882016 <andylu882016@gmail.com>
2021-03-11 16:25:21 -0800

    updated the file

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,11 @@ public class HomeController {
 	@GetMapping(path="/show")
 	public String sayHello() {
 		return "<h1>Hello My Welcome</h1><br>"+"<h2>Some change updated</h2>";
+	}
+	
+	@GetMapping(path="/updated/{name}")
+	public String domore(@Validated @PathVariable("name") final String name) {
+		
+		return name+"<h1>Welcome to Web Page </h1>";
 	}
 }
